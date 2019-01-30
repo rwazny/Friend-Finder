5bae528cf0574ed0e30ff63906f17839b309bde9
Mr Scary <ronwazny@gmail.com>
2019-01-30 12:44:54 -0500

    second

---
+++ app/data/friends.js
@@ -0,0 +1,63 @@
+// Data structure that will store the friends data
+var friendsArray = [
+  {
+    name: "Stay at Home Sadie",
+    photo: "https://i.redd.it/57qbd040k2111.jpg",
+    scores: ["1", "5", "5", "1", "5", "5", "1", "1", "1", "5"]
+  },
+  {
+    name: "Crazy Carol",
+    photo:
+      "http://profilepicturesdp.com/wp-content/uploads/2018/06/dog-funny-profile-pictures.png",
+    scores: ["2", "4", "1", "1", "1", "1", "1", "2", "1", "5"]
+  },
+  {
+    name: "Travling Tippy",
+    photo:
+      "https://static.twentytwowords.com/wp-content/uploads/hilarious_profiles_01.jpg",
+    scores: ["4", "3", "5", "1", "1", "5", "5", "1", "1", "1"]
+  },
+  {
+    name: "Sleepy Sharon",
+    photo:
+      "https://static.boredpanda.com/blog/wp-content/uploads/2017/09/funny-dog-bios-5-59ad087bd494e__700.jpg",
+    scores: ["5", "1", "2", "1", "3", "4", "1", "3", "3", "4"]
+  },
+  {
+    name: "Winston the Wine-O",
+    photo:
+      "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj236rI5pHgAhUM6IMKHb6ICtoQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F318559373634687810%2F&psig=AOvVaw0cCggqzfanRA5huRxTlWrc&ust=1548809916660852",
+    scores: ["1", "1", "5", "5", "5", "5", "5", "5", "5", "1"]
+  },
+  {
+    name: "Mr. Penny Poocher",
+    photo:
+      "http://profilepicturesdp.com/wp-content/uploads/2018/06/cool-dog-profile-pictures-7.jpg",
+    scores: ["1", "1", "5", "1", "3", "2", "5", "1", "1", "5"]
+  },
+  {
+    name: "Gage the Gamer",
+    photo:
+      "https://i.pinimg.com/originals/a0/a2/2c/a0a22ca45087b8989f2132f6bf25ce87.jpg",
+    scores: ["5", "4", "4", "2", "4", "4", "3", "2", "3", "3"]
+  },
+  {
+    name: "Bella the Boxer",
+    photo: "https://farm6.static.flickr.com/5007/5289570176_d4002b4fa6.jpg",
+    scores: ["5", "2", "3", "1", "5", "1", "3", "1", "1", "5"]
+  },
+  {
+    name: "Rita Go For a Rider",
+    photo:
+      "https://scontent-sjc3-1.cdninstagram.com/vp/ea90440a371d84d569c9333341cde146/5C81EFFA/t51.2885-19/s320x320/19367394_141487649735565_5966746508098273280_a.jpg",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+  },
+  {
+    name: "Darling Darlene",
+    photo:
+      "https://blazepress.com/.image/t_share/MTQ5NzQ4NzU3ODk3MjI2MDQy/funny-dog-online-bios-8.jpg",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+  }
+];
+
+module.exports = friendsArray;
